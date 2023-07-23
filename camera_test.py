71df3d0372d9f51fa2644e0797fe551386716c32
piotr233 <pdrabik223@gmail.com>
2023-07-23 11:58:52 +0200

    bigger rez

--- camera_test.py
+++ camera_test.py
@@ -2,7 +2,7 @@ from picamera import PiCamera
 import time
 
 camera = PiCamera()
-camera.resolution = (1920, 1080)
+camera.resolution = (2592,1944)
 camera.rotation = -90
 time.sleep(2)
 camera.capture("./test.jpg")
